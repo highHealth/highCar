525c1127992bccb1f345c80c2c928b235965cb13
paul <mogu9630@163.com>
2019-11-09 17:00:08 +0800

    update 2019/11/9 16:59 pao

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="body">
     <header-bar></header-bar>
+    <md-card>
+      <md-card-actions>
+        <div class="md-subhead">
+          <span>Loop Mode / Infinite Loop</span>
+          <span>（</span>
+          <span>无限循环模式</span>
+          <span>）</span>
+        </div>
+        <md-button class="md-icon-button"
+                  target="_blank"
+                  href="https://github.com/surmon-china/vue-awesome-swiper/blob/master/examples/20-infinite-loop.vue">
+          <md-icon>code</md-icon>
+        </md-button>
+      </md-card-actions>
+      <md-card-media>
+        <!-- swiper -->
+        <swiper :options="swiperOption" style="line-height: 280px; font-size: 5rem; text-align: center;">
+          <swiper-slide>Slide 1</swiper-slide>
+          <swiper-slide>Slide 2</swiper-slide>
+          <swiper-slide>Slide 3</swiper-slide>
+          <swiper-slide>Slide 4</swiper-slide>
+          <swiper-slide>Slide 5</swiper-slide>
+          <swiper-slide>Slide 6</swiper-slide>
+          <swiper-slide>Slide 7</swiper-slide>
+          <swiper-slide>Slide 8</swiper-slide>
+          <swiper-slide>Slide 9</swiper-slide>
+          <swiper-slide>Slide 10</swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </md-card-media>
+    </md-card>
     <div class="major">
       <div class="major-0">
         <div class="major-0-wrapper">
@@ -20,16 +53,7 @@
           <!-- <img :src="" class="iphone8"> -->
         </div>
       </div>
-      <slider animation="fade">
-        <slider-item
-          v-for="(i, index) in list"
-          :key="index"
-          :style="i"
-          @click="hello"
-        >
-          <p style="line-height: 280px; font-size: 5rem; text-align: center;">Page{{ index + 1 }}</p>
-        </slider-item>
-      </slider>
+      
       <!-- <div class="major-2">
         <div class="major-2-wrapper">
           <h2 class="headline">{{major[2].title}}</h2>
@@ -114,17 +138,26 @@ import axios from "axios";
 import footerInfo from "@/components/common/FooterInfo";
 import headerBar from "@/components/common/HeaderBar";
 import Vue from 'vue'
-import { Slider, SliderItem } from 'vue-easy-slider'
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 export default {
   data() {
     return {
       major: [],
       ads: [],
-      list: [
-        { backgroundColor: '#3f51b5', width: '100%', height: '100%' },
-        { backgroundColor: '#eee', width: '100%', height: '100%' },
-        { backgroundColor: '#f44336', width: '100%', height: '100%' },
-      ],
+      swiperOption: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+          loop: true,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }
+        }
     };
   },
   // mounted: function() {
@@ -143,8 +176,8 @@ export default {
   components: {
     footerInfo,
     headerBar,
-    Slider,
-    SliderItem,
+    swiper,
+    swiperSlide
   },
   methods: {
     hello($event) {
